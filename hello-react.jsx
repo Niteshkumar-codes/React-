693efe089219d8268d14b69ead4cf408876394d1
Nitesh Kumar <nitesh.connect242@gmail.com>
2026-08-27 21:57:23 +0530

    Create hello-react.jsx

---
+++ hello-react.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const studentName = "Nitesh";
+
+  return (
+    <div>
+      <h1>Hi, I am {studentName}</h1>
+      <p>Starting my React practice.</p>
+    </div>
+  );
+}
+
+export default App;
